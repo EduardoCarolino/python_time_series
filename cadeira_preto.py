360c2c0b8332d1bee292e11f062b010b79120e89
EduardoCarolino <163208902+EduardoCarolino@users.noreply.github.com>
2025-08-25 20:32:20 -0300

    Update cadeira_preto.py

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,1 +1,11 @@
 
+#Cabeçalho em Python
+""" Um conjunto de informações, geralmente na forma de um comentário ou metadado, localizado no início 
+do ficheiro para descrever o seu conteúdo, propósito e, em alguns casos, a sua licença, autor e data de criação """
+
+#Para o que serve
+""" Para criar uma documentação e orientar o desenvolvedor como utilizar aquele código """
+
+#Diferença de cabeçalho para docstring
+""" Docstring strings usadas para documentar módulos, classes, funções e métodos. São strings literais,
+geralmente delimitadas por aspas triplas (# ou """ """) """
